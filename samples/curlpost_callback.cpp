0fd30a031a51dc5b64a6e7d912cbf466a94cb7ad
anshul <anshul.routhu@videon-central.com>
2014-02-13 22:41:28 -0500

    Added copyrights to all the files

--- samples/curlpost_callback.cpp
+++ samples/curlpost_callback.cpp
@@ -10,12 +10,6 @@ voiceCommand
   express or implied.
 ***********************************************************/
 
-#-------------------------------------------------------------------------------
-# Copyright (c) 2014 Anshul Routhu
-# 
-# This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
-# KIND, either express or implied.
-#-------------------------------------------------------------------------------
 /*
  * curlport_callback.cpp
  *
